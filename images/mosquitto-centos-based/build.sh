8b693043a58a607186a7a4788d33dae63916f64e
Arie Lev <levinsonarie@gmail.com>
2020-03-18 21:02:27 +0200

    add -xe flag, refactor push variable function

--- images/mosquitto-centos-based/build.sh
+++ images/mosquitto-centos-based/build.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
+#!/bin/sh -xe
 
 VERSION=1.5.5
 
 if [ -z "${dockerRepo:=}" ]; then # If 'dockerRepo' is unset
   echo "'dockerRepo' variable is not set, doing nothing"
-else
-  # dockerRepo variable has some value
-  docker build \
-      --build-arg MOSQUITTO_VERSION=${VERSION} \
-      -t ${dockerRepo}/arielev/mosquitto:${VERSION} .
+else # dockerRepo variable has some value
   if [ -z "${push:=}" ]; then # If 'push' is unset
+    docker build \
+    --build-arg MOSQUITTO_VERSION=${VERSION} \
+    -t ${dockerRepo}/arielev/mosquitto:${VERSION} .
     echo "build does not contain push variable, skipping docker push";
   else
     echo "push variable is set, pushing image to ${dockerRepo}";
