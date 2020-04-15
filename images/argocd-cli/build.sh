ec656f850f439e445052908f17882c94b3947962
Arie Lev <levinsonarie@gmail.com>
2020-04-15 12:42:53 +0300

    update argocd build image script

--- images/argocd-cli/build.sh
+++ images/argocd-cli/build.sh
@@ -1,8 +1,20 @@
-#!/bin/sh
+#!/bin/sh -xe
 
 VERSION=v1.0.2
 
-docker build \
-    --build-arg ARGOCD_CLI_VERSION=${VERSION} \
-    -t ${1}/arielev/argocd-cli:${VERSION} .
-docker push ${1}/arielev/argocd-cli:${VERSION}
+if [ -z "${dockerRepo:=}" ]; then # If 'dockerRepo' is unset
+  echo "'dockerRepo' variable is not set, doing nothing"
+else # dockerRepo variable has some value
+  if [ -z "${push:=}" ]; then # If 'push' is unset
+    # Add --network host so docker in docker will not fail
+    docker build \
+      --network host \
+      --build-arg ARGOCD_CLI_VERSION=${VERSION} \
+      -t ${dockerRepo}/arielev/argocd-cli:${VERSION} .
+    echo "build does not contain push variable, skipping docker push";
+  else
+    echo "push variable is set, pushing image to ${dockerRepo}";
+    # Only jenkins should push images
+    docker push ${dockerRepo}/arielev/argocd-cli:${VERSION}
+  fi
+fi
