85656b19dfc9df4bc355315bce210179793ab510
Arie Lev <levinsonarie@gmail.com>
2020-08-20 19:03:32 +0300

    fix kubectl docker image build process

--- images/kubectl/README.md
+++ images/kubectl/README.md
@@ -10,10 +10,6 @@ Current built version: v1.18.8
 Build
 -----
 Execute to build **latest** version:  
-```bash
-dockerRepo=docker.io
-
-./build.sh ${dockerRepo}
+```shell script
+dockerRepo=docker.io ./build.sh ${dockerRepo}
 ```
-
-For deploying the image on Kubernetes view [the helm chart](https://github.com/ArieLevs/Kubernetes-Helm-Charts/tree/master/charts/mosquitto)
--- images/kubectl/build.sh
+++ images/kubectl/build.sh
@@ -1,8 +1,20 @@
-#!/bin/sh
+#!/bin/sh -xe
 
 LATEST_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 
-docker build \
+if [ -z "${dockerRepo:=}" ]; then # If 'dockerRepo' is unset
+  echo "'dockerRepo' variable is not set, doing nothing"
+else # dockerRepo variable has some value
+  if [ -z "${push:=}" ]; then # If 'push' is unset
+    # Add --network host so docker in docker will not fail
+    docker build \
+    --network host \
     --build-arg KUBECTL_VERSION=${LATEST_VERSION} \
-    -t ${1}/arielev/kubectl:${LATEST_VERSION} .
-docker push ${1}/arielev/kubectl:${LATEST_VERSION}
+    -t ${dockerRepo}/arielev/kubectl:${LATEST_VERSION} .
+    echo "build does not contain push variable, skipping docker push";
+  else
+    echo "push variable is set, pushing image to ${dockerRepo}";
+    # Only ci process should push images
+    docker push ${dockerRepo}/arielev/kubectl:${LATEST_VERSION}
+  fi
+fi
